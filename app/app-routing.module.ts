2952b960264339bd593329eb2f824c9a24e406f8
KarolZerna <karol.zern@gmail.com>
2022-12-03 07:59:41 -0400

    test

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -8,10 +8,12 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ListaDentistasComponent } from './components/lista-dentistas/lista-dentistas.component';
 import { RegistrarDentistaComponent } from './components/registrar-dentista/registrar-dentista.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   { path: 'inicio', component: LandingComponent},
   { path: '', redirectTo: 'inicio', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
   { path: 'historialPaciente/:id', component: HistorialPacienteComponent},
   { path: 'listaPacientes', component: ListaPacientesComponent },
   { path: 'registrarPaciente', component: RegistrarPacienteComponent },
